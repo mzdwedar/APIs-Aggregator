098664c619363247088e73f90c7b64d51e583818
Mohamed Dwedar <mzd97@hotmail.com>
2021-10-14 11:12:50 +0300

    Add files via upload

---
+++ const.py
@@ -0,0 +1,8 @@
+
+news_api = {"key": "a3c2e8f1ced7418f82a0699a0a1a9f6e"}
+
+
+reddit_api = {"public key":"Bjisg3DsCBr3ezOFvRvsdw",
+            "secret":"VV6xEpm-sDsCJey0IBH0BeRk4BN8aQ",
+            "username":"mzdwedar",
+            "password":"Mz@231197"}
